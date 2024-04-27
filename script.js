c9160a937f9cc3f5f4be1de81c8fcd504a80ae11
abaines1 <abaines1@vols.utk.edu>
2024-04-27 10:45:17 -0400

    updates

--- script.js
+++ script.js
@@ -229,5 +229,53 @@ function resetGame() {
 
 }
 
+/* Variables storing an audio objects to make the various sounds.  See how it's used for the 'click' sound in the provided function below.  */
+let clickAudio = new Audio('audio/click.wav');
+let matchAudio = new Audio('audio/match.wav');
+let winAudio = new Audio('audio/win.wav')
+
+/* OVERVIEW:
+Attaches an mouseclick listener to a card (i.e. onclick), flips the card when clicked, and calls the function 'onCardFlipped' after the flip is complete.
+
+INPUT/OUPUT
+The 'cardObject' parameter is a custom card object we created in the 'createCards' function.
+
+This function will make the card element associated with 'cardObject' clickable and call onCardFlipped with that cardObject after the flip is complete. */
+function flipCardWhenClicked(cardObject) {
+  // Adds an "onclick" attribute/listener to the element that will call the function below.
+  cardObject.element.onclick = function() {
+    // THE CODE BELOW RUNS IN RESPONSE TO A CLICK.
+
+    // Card is already flipped, return.
+    if (cardObject.element.classList.contains("flipped")) {
+      return;
+    }
+
+    // Play the "click" sound.
+    clickAudio.play();
+
+    // Add the flipped class immediately after a card is clicked.
+    cardObject.element.classList.add("flipped");
+
+    // Wait 500 milliseconds (1/2 of a second) for the flip transition to complete and then call onCardFlipped.
+    setTimeout(function() {
+      // THE CODE BELOW RUNS AFTER a 500ms delay.
+      onCardFlipped(cardObject);
+    }, 500);
+  };
+}
+/* Set up the game. This functions calls createCards() and adds onclicks to each card created. */
+function setUpGame() {
+  let cardObjects = 
+    createCards(document.getElementById("card-container"), shuffleCardImageClasses());
+
+  if (cardObjects != null) {
+    for (let i = 0; i < cardObjects.length; i++) {
+      flipCardWhenClicked(cardObjects[i]);
+    }
+  }
+}
+
+
 // ⛔️ Set up the game. Do not edit below this line! ⛔
 setUpGame();
